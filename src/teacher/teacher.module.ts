f56b522b4cbb11fa2c7e6515972f1ce71936dad5
slaveeks <chernyshev2001@icloud.com>
2023-08-11 15:54:56 +0300

    Some fixes with TypeORM

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TeacherController } from './teacher.controller';
 import { TeacherService } from './teacher.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TeacherModel } from './teacher.model';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TeacherModel])],
   controllers: [TeacherController],
   providers: [TeacherService],
 })
